056cb5e979a59b846f6351f714aaacc9158bc526
Daniel Whiffing <danielwhiffing@gmail.com>
2022-10-02 17:32:05 -0400

    Tweaks AI

--- src/constants.ts
+++ src/constants.ts
@@ -4,18 +4,18 @@ export const ROUND_DURATION = 10
 export const ROUND_DELAY = 1000
 export const AI_CONFIG: Record<string, AIConfig> = {
   EASY: {
-    startWait: 3,
-    endWait: 1,
+    startWait: 4,
+    endWait: 2,
     cardSlice: 5,
   },
   MEDIUM: {
-    startWait: 5,
-    endWait: 10,
+    startWait: 6,
+    endWait: 5,
     cardSlice: 10,
   },
   HARD: {
-    startWait: 8,
-    endWait: 20,
+    startWait: 6,
+    endWait: 10,
     cardSlice: 19,
   },
 }
